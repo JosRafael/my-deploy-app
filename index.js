6bf94c3fab594b3cfeadeb179f080677a2e04aea
JosRafael <rafaelmatiashue@gmail.com>
2024-06-20 15:44:30 -0300

    ok

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const corsOptions = {
+    origin: '*', // Aqui você pode especificar os domínios permitidos
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+};
+app.use(cors(corsOptions));
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    if (error.code === 'P2002') {
+      res.status(400).json({ error: 'Email não exists' });
+    } else {
+      console.error('Error creating user:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
